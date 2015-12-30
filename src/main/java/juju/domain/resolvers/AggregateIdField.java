7c92b02b345891adb9dce62107f9db86e17902dc
rucka <rucka@tiscalinet.it>
2015-12-30 17:58:49 +0100

    add missing file

---
+++ src/main/java/juju/domain/resolvers/AggregateIdField.java
@@ -0,0 +1,12 @@
+package juju.domain.resolvers;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface AggregateIdField {
+    public String fieldname() default "";
+}
