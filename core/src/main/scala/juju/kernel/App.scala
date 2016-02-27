2d6d71fe8061115a331041d8ea4b969ddf9b4ea9
rucka <rucka@tiscalinet.it>
2016-02-27 07:41:13 +0100

    make app appConfig overridable

--- core/src/main/scala/juju/kernel/App.scala
+++ core/src/main/scala/juju/kernel/App.scala
@@ -21,7 +21,7 @@ trait App extends juju.kernel.Bootable {
   private var roleApps: Map[String, (ModulePropsFactory[_ <: Module], Config, AfterModuleCreation)] = Map.empty
   private var roleSystems: Map[String, Try[(ActorSystem, ActorRef)]] = Map.empty
 
-  private def appConfig() = {
+  protected def appConfig() = {
     ConfigFactory.defaultApplication()
       .withFallback(ConfigFactory.parseString("juju.timeout = 5s"))
       .withFallback(ConfigFactory.parseString("akka.cluster.roles = []"))
