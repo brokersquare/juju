ed0a0b3fbc42a5892a00513791ff6ec1a23ac8bb
rucka <rucka@tiscalinet.it>
2016-03-12 16:25:40 +0100

    reference actors renamed

--- juju-cluster/src/test/scala/juju/infrastructure/cluster/ClusterEventBusSpec.scala
+++ juju-cluster/src/test/scala/juju/infrastructure/cluster/ClusterEventBusSpec.scala
@@ -10,23 +10,23 @@ import juju.testkit.infrastructure.EventBusSpec
 class ClusterEventBusSpec extends ClusterDomainSpec("ClusterEventBus") with EventBusSpec {
   override def withEventBus(subscriber: ActorRef, subscribedEvents : Seq[Class[_]])(action : ActorRef => Unit) = {
     val events = subscribedEvents map(_.getSimpleName)
-    var busRef: ActorRef = null
+    var bus: ActorRef = null
     val mediator = DistributedPubSub(system).mediator
 
     try {
-      busRef = system.actorOf(EventBus.props(tenant))
+      bus = system.actorOf(EventBus.props(tenant))
       val subscriptionGroup = "testGroup"
       events.foreach { e =>
         mediator ! Subscribe(EventBus.nameWithTenant(tenant, e), Some(EventBus.nameWithTenant(tenant, subscriptionGroup)), subscriber)
       }
 
-      action(busRef)
+      action(bus)
     } finally {
       events.foreach { e =>
         mediator ! Unsubscribe(EventBus.nameWithTenant(tenant, e), None, subscriber)
       }
 
-      if (busRef != null) busRef ! PoisonPill
+      if (bus != null) bus ! PoisonPill
     }
   }
 }
